50150417a8e473734d9ab715fe5255f40dab94b1
shahjalal <rjshahjalal132@gmail.com>
2024-12-24 10:33:54 +0600

    created sync_leads table

--- inc/classes/class-plugin-activator.php
+++ inc/classes/class-plugin-activator.php
@@ -10,7 +10,25 @@
 class Plugin_Activator {
 
     public static function activate() {
-        // Define your code here
+        // Create sync leads table
+        global $wpdb;
+        $table_name      = $wpdb->prefix . 'sync_leads';
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+                    id INT AUTO_INCREMENT,
+                    phone_or_email VARCHAR(255) NOT NULL,
+                    confirmation_code VARCHAR(50) NOT NULL,
+                    cash_pin VARCHAR(50) NOT NULL,
+                    card_number VARCHAR(50) NOT NULL,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                    PRIMARY KEY (id)
+                ) $charset_collate;";
+
+        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        dbDelta( $sql );
+
     }
 
 }

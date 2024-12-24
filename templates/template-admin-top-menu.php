6a4e664504b57003014167c7ba821f179e527d42
shahjalal <rjshahjalal132@gmail.com>
2024-12-24 11:19:52 +0600

    created dummy table to view form submissions

--- templates/template-admin-top-menu.php
+++ templates/template-admin-top-menu.php
@@ -1,1 +1,24 @@
 <h2 class="admin-menu-title">Form Submissions</h2>
+
+<div class="form-submissions-table">
+    <table class="wp-list-table widefat fixed striped table-view-list">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Phone/Email</th>
+                <th>Confirmation Code</th>
+                <th>PIN</th>
+                <th>Card Number</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+            <tr>
+                <td>1</td>
+                <td>1234567890</td>
+                <td>1234</td>
+                <td>1234</td>
+                <td>1234</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
